ea7b9611a934b53f8d7167b7810daeabaeb36eb3
Mahendra Hegde <hegde.mahendra@tcs.com>
2019-05-07 13:40:04 +0530

    typos fixed

--- hadoop-hive-cassandra/start.sh
+++ hadoop-hive-cassandra/start.sh
@@ -1,3 +1,4 @@
+#hackerrank run->install can use this script
 if [ `id -u` -ne 0 ]; then
     echo "You need root privileges to run this script(try sudo)"
     exit 1
@@ -8,9 +9,11 @@ bash -c "cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys"
 chmod 600 /root/.ssh/authorized_keys
 service ssh restart
 
-hdfs namenode -format
-start-all.sh
+echo 'yes' | /opt/mdp/hadoop-2.7.6/bin/hdfs namenode -format
+echo 'yes \n yes \n' | /opt/mdp/hadoop-2.7.6/sbin/start-all.sh
 /opt/mdp/hadoop-2.7.6/bin/hdfs dfs -mkdir -p /user/hive/warehouse
 /opt/mdp/hadoop-2.7.6/bin/hdfs dfs -mkdir /tmp
 /opt/mdp/hadoop-2.7.6/bin/hdfs dfs -chmod g+w /user/hive/warehouse
 /opt/mdp/hadoop-2.7.6/bin/hdfs dfs -chmod g+w /tmp
+
+service cassandra start
