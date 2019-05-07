cbd288f6672e4c4f58e5a22f0ea9f3032286e2cc
Mahendra Hegde <hegde.mahendra@tcs.com>
2019-05-07 10:55:15 +0530

    added hive setup instructions

--- hadoop-hive-cassandra/start.sh
+++ hadoop-hive-cassandra/start.sh
@@ -1,9 +1,16 @@
-sudo sudo PATH=$PATH
+if [ `id -u` -ne 0 ]; then
+    echo "You need root privileges to run this script(try sudo)"
+    exit 1
+fi
+
 bash -c  'echo "\n" | ssh-keygen -q -t rsa -P "" -f /root/.ssh/id_rsa'
 bash -c "cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys"
 chmod 600 /root/.ssh/authorized_keys
 service ssh restart
-/opt/mdp/hadoop-2.7.6/bin/hadoop dfs -mkdir -p /user/hive/warehouse
-/opt/mdp/hadoop-2.7.6/bin/hadoop dfs -mkdir /tmp
-/opt/mdp/hadoop-2.7.6/bin/hadoop dfs -chmod g+w /user/hive/warehouse
-/opt/mdp/hadoop-2.7.6/bin/hadoop dfs -chmod g+w /tmp
+
+hdfs namenode -format
+start-all.sh
+/opt/mdp/hadoop-2.7.6/bin/hdfs dfs -mkdir -p /user/hive/warehouse
+/opt/mdp/hadoop-2.7.6/bin/hdfs dfs -mkdir /tmp
+/opt/mdp/hadoop-2.7.6/bin/hdfs dfs -chmod g+w /user/hive/warehouse
+/opt/mdp/hadoop-2.7.6/bin/hdfs dfs -chmod g+w /tmp
